c733f77264955002c8bdfbf7e92eff2fdf1b13a6
sorcho <mattiascotellaro@tutamail.com>
2026-06-19 16:53:29 +0200

    added authroute and middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
+import cookieParser from "cookie-parser"
 import { syncDatabase } from "./models/Database.js";
+import { authRouter } from "./routes/authRouter.js";
 
 const app = express();
 const PORT = 3000;
@@ -11,5 +13,10 @@ try{
 } catch (error) {
     console.error('Error: ' + error);
 }
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(authRouter);
 
 app.listen(PORT);
