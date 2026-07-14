0375969bcd677a89cf64d31ea44f35e65b5ba42f
sorcho <mattiascotellaro@tutamail.com>
2026-07-14 15:01:13 +0200

    simplified the calls and better managed the returns. fortified the cookie against csrf

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -3,18 +3,41 @@ import bcrypt from "bcrypt";
 import { User } from "./../models/Database.js"
 
 export class AuthController {
-    static newUser(req, res) {
-        let user = new User({
-            username: req.body.username,
-            password: req.body.password
-        });
+    static async newUser(req, res) {
+        try {
+            let user = new User({
+                username: req.body.username,
+                password: req.body.password
+            });
 
-        return user.save();
+            await user.save();
+
+            res.status(201).json({ message: "Registration completed." })
+        } catch (err) {
+            res.status(400).json({ message: "Error during the registration." })
+        }
     }
 
     static async checkCreds(req, res) {
         const user = await User.findOne({ where: { username: req.body.username } });
-        if (user && bcrypt.compareSync(req.body.password, user.password)) return user;
+
+        if (!user) {
+            return res.status(400).json({ message: "No user found with given username." });
+        }
+
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.status(400).json({ message: "Password didn't match." });
+        }
+
+        const token = AuthController.issueToken(user.id);
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 24 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json({ message: 'Logged in successfully.' });
     }
 
     static issueToken(id) {
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -3,26 +3,10 @@ import { AuthController } from "../controllers/AuthController.js";
 
 export const authRouter = express.Router();
 
-authRouter.post("/login", async (req, res) => {
-    let user = await AuthController.checkCreds(req, res);
-
-    if(!user) {
-        res.status(400).json({message: "Invalid credentials"});
-    }
-
-    const token = AuthController.issueToken(user.id);
-    res.cookie('token', token, {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000
-    })
-    res.status(200).send();
-
+authRouter.post("/login", (req, res) => {
+    AuthController.checkCreds(req, res);
 });
 
-authRouter.post("/signup", async (req, res, next) => {
-    AuthController.newUser(req, res).then(() => {
-        res.status(201).send();
-    }).catch((err) => {
-        res.status(400).json({ error_message: err.errors[0].message, error_type: err.errors[0].type });
-    });
+authRouter.post("/signup", (req, res, next) => {
+    AuthController.newUser(req, res)
 })
