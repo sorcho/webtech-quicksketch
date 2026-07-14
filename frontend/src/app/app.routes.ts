ca80940c049e86ac381e193beaca86b836255014
sorcho <mattiascotellaro@tutamail.com>
2026-07-14 17:32:53 +0200

    deleted auto generated angular example code

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './homepage/homepage';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { Profile } from './profile/profile';
+import { Guessers } from './guessers/guessers';
+import { Drawers } from './drawers/drawers';
+import { Sketches } from './sketches/sketches';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Homepage',
+        component: Homepage
+    },
+    {
+        path: 'login',
+        title: 'Login',
+        component: Login
+    },
+    {
+        path: 'signup',
+        title: 'Registration',
+        component: Signup
+    },
+    {
+        path: 'profile',
+        title: 'Profile',
+        component: Profile
+    },
+    {
+        path: 'leaderboard/best_guessers',
+        title: 'Best guessers',
+        component: Guessers
+    },
+    {
+        path: 'leaderboard/best_drawers',
+        title: 'Best drawers',
+        component: Drawers
+    },
+    {
+        path: 'sketches',
+        title: 'Sketches',
+        component: Sketches
+    }
+];
