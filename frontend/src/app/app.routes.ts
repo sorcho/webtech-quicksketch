b56c31e4bc1c3f20d8af9048d571c8b8a4b1feae
sorcho <mattiascotellaro@tutamail.com>
2026-07-18 16:45:40 +0200

    added authentication route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Auth } from './auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'authenticate',
+        component: Auth,
+        title: 'Authenticate'
+    }
+];
